a0fcd351b7b69684baa5e1010d7938ad6392dd42
Massimo Torquati <torquati@di.unipi.it>
2010-02-03 16:26:35 +0000

    new fastflow version 1.0.0 release candidate 1 * some more tests * some more applications * improved interface * improved memory allocator * new accelerator mode (beta version) * minor fixes and cleanup

--- nqueens/nq_ff_acc.cpp
+++ nqueens/nq_ff_acc.cpp
@@ -159,61 +159,6 @@ typedef struct {
 
 static unsigned long streamlen=0;
 
-/* ************************************************ */
-
-
-/* Algorithm:
-
-   We calculate one-half the solutions, then flip the results over
-   the "Y axis" of the board.  Every solution can be reflected that
-   way to generate another unique solution (assuming the board size
-   isn't 1 x 1).  That's because a solution cannot be symmetrical
-   across the Y-axis (because you can't have two queens in the same
-   horizontal row).  A solution also cannot consist of queens 
-   down the middle column of a board with an odd number of columns,
-   since you can't have two queens in the same vertical row.
-
-   This is a backtracking algorithm.  We place a queen in the top
-   row, then note the column and diagonals it occupies.  We then 
-   place a queen in the next row down, taking care not to place it
-   in the same column or diagonal.  We then update the occupied
-   columns & diagonals & move on to the next row.  If no position
-   is open in the next row, we back track to the previous row & move
-   the queen over to the next available spot in its row & the process
-   starts over again.
-*/
-
-/* Print the results at the end of the run */
-void printResults(time_t* pt1, time_t* pt2)
-{
-    double secs;
-    int hours , mins, intsecs;
-
-    printf("End: \t%s", ctime(pt2));
-    secs = difftime(*pt2, *pt1);
-    intsecs = (int)secs;
-    printf("Calculations took %d second%s.\n", intsecs, (intsecs == 1 ? "" : "s"));
-
-    /* Print hours, minutes, seconds */
-    hours = intsecs/3600;
-    intsecs -= hours * 3600;
-    mins = intsecs/60;
-    intsecs -= mins * 60;
-    if (hours > 0 || mins > 0) 
-    {
-        printf("Equals ");
-        if (hours > 0) 
-        {
-            printf("%d hour%s, ", hours, (hours == 1) ? "" : "s");
-        }
-        if (mins > 0)
-        {           
-            printf("%d minute%s and ", mins, (mins == 1) ? "" : "s");
-        }
-        printf("%d second%s.\n", intsecs, (intsecs == 1 ? "" : "s"));
-
-    }
-}
 
 class Worker: public ff_node {
 private:
@@ -320,148 +265,207 @@ public:
 };
 
 
+/* ********** end FastFlow specific ************** */
 
 
-// the load-balancer filter
-class Emitter: public ff_node {
-private:
-    void streamit(int board_size, int depth) {
-        int aQueenBitCol[MAX_BOARDSIZE]; /* marks colummns which already have queens */
-        int aQueenBitPosDiag[MAX_BOARDSIZE]; /* marks "positive diagonals" which already have queens */
-        int aQueenBitNegDiag[MAX_BOARDSIZE]; /* marks "negative diagonals" which already have queens */
-        int aStack[MAX_BOARDSIZE + 2]; /* we use a stack instead of recursion */
-        register int* pnStack;
-        
-        register int numrows = 0; /* numrows redundant - could use stack */
-        register unsigned int lsb; /* least significant bit */
-        register unsigned int bitfield; /* bits which are set mark possible positions for a queen */
-        int i;
-        int odd = board_size & 1; /* 0 if board_size even, 1 if odd */
-        int board_minus = board_size - 1; /* board size - 1 */
-        int mask = (1 << board_size) - 1; /* if board size is N, mask consists of N 1's */
-        
-        
-        /* Initialize stack */
-        aStack[0] = -1; /* set sentinel -- signifies end of stack */
-        
-        /* NOTE: (board_size & 1) is true iff board_size is odd */
-        /* We need to loop through 2x if board_size is odd */
-        for (i = 0; i < (1 + odd); ++i) {
-            /* We don't have to optimize this part; it ain't the 
-               critical loop */
-            bitfield = 0;
-            if (0 == i) {
-                /* Handle half of the board, except the middle
-                   column. So if the board is 5 x 5, the first
-                   row will be: 00011, since we're not worrying
-                   about placing a queen in the center column (yet).
-                */
-                int half = board_size>>1; /* divide by two */
-                /* fill in rightmost 1's in bitfield for half of board_size
-                   If board_size is 7, half of that is 3 (we're discarding the remainder)
-                   and bitfield will be set to 111 in binary. */
-                bitfield = (1 << half) - 1;
-                pnStack = aStack + 1; /* stack pointer */
+/* Algorithm:
+
+   We calculate one-half the solutions, then flip the results over
+   the "Y axis" of the board.  Every solution can be reflected that
+   way to generate another unique solution (assuming the board size
+   isn't 1 x 1).  That's because a solution cannot be symmetrical
+   across the Y-axis (because you can't have two queens in the same
+   horizontal row).  A solution also cannot consist of queens 
+   down the middle column of a board with an odd number of columns,
+   since you can't have two queens in the same vertical row.
+
+   This is a backtracking algorithm.  We place a queen in the top
+   row, then note the column and diagonals it occupies.  We then 
+   place a queen in the next row down, taking care not to place it
+   in the same column or diagonal.  We then update the occupied
+   columns & diagonals & move on to the next row.  If no position
+   is open in the next row, we back track to the previous row & move
+   the queen over to the next available spot in its row & the process
+   starts over again.
+*/
+void Nqueen(int nworkers, int depth, int board_size)
+{
+    int aQueenBitRes[MAX_BOARDSIZE]; /* results */
+    int aQueenBitCol[MAX_BOARDSIZE]; /* marks colummns which already have queens */
+    int aQueenBitPosDiag[MAX_BOARDSIZE]; /* marks "positive diagonals" which already have queens */
+    int aQueenBitNegDiag[MAX_BOARDSIZE]; /* marks "negative diagonals" which already have queens */
+    int aStack[MAX_BOARDSIZE + 2]; /* we use a stack instead of recursion */
+    register int* pnStack;
+
+
+    register int numrows = 0; /* numrows redundant - could use stack */
+    register unsigned int lsb; /* least significant bit */
+    register unsigned int bitfield; /* bits which are set mark possible positions for a queen */
+    int i;
+    int odd = board_size & 1; /* 0 if board_size even, 1 if odd */
+    //int board_minus = board_size - 1; /* board size - 1 */
+    int mask = (1 << board_size) - 1; /* if board size is N, mask consists of N 1's */
+
+
+    ff_farm<> farm(true /* set accelerator mode */, 8192);    
+    std::vector<ff_node *> w;
+    for(int i=0;i<nworkers;++i) w.push_back(new Worker(board_size));
+    farm.add_workers(w);
+
+    farm.run();
+
+    /* Initialize stack */
+    aStack[0] = -1; /* set sentinel -- signifies end of stack */
+
+    /* NOTE: (board_size & 1) is true iff board_size is odd */
+    /* We need to loop through 2x if board_size is odd */
+    for (i = 0; i < (1 + odd); ++i)
+    {
+        /* We don't have to optimize this part; it ain't the 
+           critical loop */
+        bitfield = 0;
+        if (0 == i)
+        {
+            /* Handle half of the board, except the middle
+               column. So if the board is 5 x 5, the first
+               row will be: 00011, since we're not worrying
+               about placing a queen in the center column (yet).
+            */
+            int half = board_size>>1; /* divide by two */
+            /* fill in rightmost 1's in bitfield for half of board_size
+               If board_size is 7, half of that is 3 (we're discarding the remainder)
+               and bitfield will be set to 111 in binary. */
+            bitfield = (1 << half) - 1;
+            pnStack = aStack + 1; /* stack pointer */
+    
+            aQueenBitRes[0] = 0;
+            aQueenBitCol[0] = aQueenBitPosDiag[0] = aQueenBitNegDiag[0] = 0;
+        }
+        else
+        {
+            /* Handle the middle column (of a odd-sized board).
+               Set middle column bit to 1, then set
+               half of next row.
+               So we're processing first row (one element) & half of next.
+               So if the board is 5 x 5, the first row will be: 00100, and
+               the next row will be 00011.
+            */
+            bitfield = 1 << (board_size >> 1);
+            numrows = 1; /* prob. already 0 */
+
+            /* The first row just has one queen (in the middle column).*/
+            aQueenBitRes[0] = bitfield;
+            aQueenBitCol[0] = aQueenBitPosDiag[0] = aQueenBitNegDiag[0] = 0;
+            aQueenBitCol[1] = bitfield;
+
+            /* Now do the next row.  Only set bits in half of it, because we'll
+               flip the results over the "Y-axis".  */
+            aQueenBitNegDiag[1] = (bitfield >> 1);
+            aQueenBitPosDiag[1] = (bitfield << 1);
+            pnStack = aStack + 1; /* stack pointer */
+            *pnStack++ = 0; /* we're done w/ this row -- only 1 element & we've done it */
+            bitfield = (bitfield - 1) >> 1; /* bitfield -1 is all 1's to the left of the single 1 */
+        }
+
+        /* this is the critical loop */
+        for (;;)
+        {
+            /* could use 
+               lsb = bitfield ^ (bitfield & (bitfield -1)); 
+               to get first (least sig) "1" bit, but that's slower. */
+            lsb = -((signed)bitfield) & bitfield; /* this assumes a 2's complement architecture */
+            if (0 == bitfield) 
+            {
+                bitfield = *--pnStack; /* get prev. bitfield from stack */
+                if (pnStack == aStack) { /* if sentinel hit.... */
+                    break ;
+                }
+                --numrows;
+                continue;
+            }
+            bitfield &= ~lsb; /* toggle off this bit so we don't try it again */
+
+            aQueenBitRes[numrows] = lsb; /* save the result */
+
+            if (numrows == (depth-1)) {
+                task_t * task = (task_t*)ffalloc.malloc(sizeof(task_t));
+                task->numrow           = numrows;
+                task->aQueenBitCol     = aQueenBitCol[numrows];
+                task->aQueenBitNegDiag = aQueenBitNegDiag[numrows];
+                task->aQueenBitPosDiag = aQueenBitPosDiag[numrows];
                 
-                aQueenBitCol[0] = aQueenBitPosDiag[0] = aQueenBitNegDiag[0] = 0;
-            } else {
+                ++streamlen;
+                farm.offload(task);
+                    
+                bitfield = *--pnStack;
+                if (pnStack == aStack) break;  /* if sentinel hit.... */
                 
-                /* Handle the middle column (of a odd-sized board).
-                   Set middle column bit to 1, then set
-                   half of next row.
-                   So we're processing first row (one element) & half of next.
-                   So if the board is 5 x 5, the first row will be: 00100, and
-                   the next row will be 00011.
-                */
-                bitfield = 1 << (board_size >> 1);
-                numrows = 1; /* prob. already 0 */
-                
-                /* The first row just has one queen (in the middle column).*/
-                //aQueenBitRes[0] = bitfield;
-                aQueenBitCol[0] = aQueenBitPosDiag[0] = aQueenBitNegDiag[0] = 0;
-                aQueenBitCol[1] = bitfield;
-                
-                /* Now do the next row.  Only set bits in half of it, because we'll
-                   flip the results over the "Y-axis".  */
-                aQueenBitNegDiag[1] = (bitfield >> 1);
-                aQueenBitPosDiag[1] = (bitfield << 1);
-                pnStack = aStack + 1; /* stack pointer */
-                *pnStack++ = 0; /* we're done w/ this row -- only 1 element & we've done it */
-                bitfield = (bitfield - 1) >> 1; /* bitfield -1 is all 1's to the left of the single 1 */
-                depth++; // for a odd-sized board we have to go a step forward
+                --numrows;
+                continue;
             }
             
-            /* this is the critical loop */
-            for (;;) {
-                /* could use 
-                   lsb = bitfield ^ (bitfield & (bitfield -1)); 
-                   to get first (least sig) "1" bit, but that's slower. */
-                lsb = -((signed)bitfield) & bitfield; /* this assumes a 2's complement architecture */
-                if (0 == bitfield) {
-                    bitfield = *--pnStack; /* get prev. bitfield from stack */
-                    if (pnStack == aStack) { /* if sentinel hit.... */
-                        break ;
-                    }
-                    --numrows;
-                    continue;
-                }
-                bitfield &= ~lsb; /* toggle off this bit so we don't try it again */
-                
-                assert(numrows<board_minus);
-                
-                if (numrows == (depth-1)) {
-                    task_t * task = (task_t*)ffalloc.malloc(sizeof(task_t));
-                    task->numrow           = numrows;
-                    task->aQueenBitCol     = aQueenBitCol[numrows];
-                    task->aQueenBitNegDiag = aQueenBitNegDiag[numrows];
-                    task->aQueenBitPosDiag = aQueenBitPosDiag[numrows];
-                    
-                    ++streamlen;
-                    ff_send_out(task); // FIX retry fallback?????
-                    
-                    bitfield = *--pnStack;
-                    if (pnStack == aStack) break;  /* if sentinel hit.... */
-                    --numrows;
-                    continue;
-                }
-
-                int n = numrows++;
-                aQueenBitCol[numrows] = aQueenBitCol[n] | lsb;
-                aQueenBitNegDiag[numrows] = (aQueenBitNegDiag[n] | lsb) >> 1;
-                aQueenBitPosDiag[numrows] = (aQueenBitPosDiag[n] | lsb) << 1;
-                
-                
-                *pnStack++ = bitfield;
-                
-                /* We can't consider positions for the queen which are in the same
-                   column, same positive diagonal, or same negative diagonal as another
-                   queen already on the board. */
-                bitfield = mask & ~(aQueenBitCol[numrows] | aQueenBitNegDiag[numrows] | aQueenBitPosDiag[numrows]);
-            }
+            int n = numrows++;
+            aQueenBitCol[numrows] = aQueenBitCol[n] | lsb;
+            aQueenBitNegDiag[numrows] = (aQueenBitNegDiag[n] | lsb) >> 1;
+            aQueenBitPosDiag[numrows] = (aQueenBitPosDiag[n] | lsb) << 1;
+            
+            
+            *pnStack++ = bitfield;
+            
+            /* We can't consider positions for the queen which are in the same
+               column, same positive diagonal, or same negative diagonal as another
+               queen already on the board. */
+                     bitfield = mask & ~(aQueenBitCol[numrows] | aQueenBitNegDiag[numrows] | aQueenBitPosDiag[numrows]);
         }
     }
 
-public:
-    Emitter(int boardsize, int depth):boardsize(boardsize),depth(depth) {};
+    farm.offload((void *)FF_EOS);
+    farm.wait();
+    for(int i=0;i<nworkers;++i) g_numsolutions += ((Worker *)w[i])->getSolutions();
 
-    int svc_init() {
-        if (ffalloc.registerAllocator()<0) {
-            error("Emitter, registerAllocator fails\n");
-            return -1;
-        }
-        return 0;
-    }
-
-    void * svc(void *) {
-        streamit(boardsize,depth); 
-        return NULL;
-    }
+    /* multiply solutions by two, to count mirror images */
+    //g_numsolutions *= 2;
     
-private:
-    int  boardsize;
-    int  depth;
-};
+    printf("\nFastFlow's stats:\n");
+    for(int i=0;i<nworkers;++i)
+        printf("Worker id=%d, nsol=%lld\n", 
+               i, ((Worker *)w[i])->getSolutions());
+    printf("Stream Len= %ld\n", streamlen);
+    printf("Time: %g (ms)\n", farm.ffTime());
+    farm.ffStats(std::cout);
+}
 
+/* Print the results at the end of the run */
+void printResults(time_t* pt1, time_t* pt2)
+{
+    double secs;
+    int hours , mins, intsecs;
+
+    printf("End: \t%s", ctime(pt2));
+    secs = difftime(*pt2, *pt1);
+    intsecs = (int)secs;
+    printf("Calculations took %d second%s.\n", intsecs, (intsecs == 1 ? "" : "s"));
+
+    /* Print hours, minutes, seconds */
+    hours = intsecs/3600;
+    intsecs -= hours * 3600;
+    mins = intsecs/60;
+    intsecs -= mins * 60;
+    if (hours > 0 || mins > 0) 
+    {
+        printf("Equals ");
+        if (hours > 0) 
+        {
+            printf("%d hour%s, ", hours, (hours == 1) ? "" : "s");
+        }
+        if (mins > 0)
+        {           
+            printf("%d minute%s and ", mins, (mins == 1) ? "" : "s");
+        }
+        printf("%d second%s.\n", intsecs, (intsecs == 1 ? "" : "s"));
+
+    }
+}
 
 
 
@@ -469,7 +473,7 @@ private:
 int main(int argc, char** argv) {
     time_t t1, t2;
     int boardsize;
-    int nworkers=2;  // FIX
+    int nworkers=2;  // FIX 
     int depth=2;     // FIX
 
     if (argc == 1 || argc > 4) {
@@ -479,7 +483,7 @@ int main(int argc, char** argv) {
         printf("\ttorquati@di.unipi.it or massimotor@gmail.com\n");
         printf("This program calculates the total number of solutions to the N Queens problem.\n");
         /* user must pass in size of board */
-        printf("Usage: nq_ff <width of board> [<num-workers> <depth>]\n"); 
+        printf("Usage: nq_ff_acc <width of board> [<num-workers> <depth>]\n"); 
         return 0;
     }
 
@@ -499,28 +503,22 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-    // init allocator
+    // init FastFlow allocator
     ffalloc.init();
-    ff_farm<> farm(false, 8192);    
-    Emitter E(boardsize, depth);
-    farm.add_emitter(&E);
+    if (ffalloc.registerAllocator()<0) {
+        error("Emitter, registerAllocator fails\n");
+        return -1;
+    }
 
-    std::vector<ff_node *> w;
-    for(int i=0;i<nworkers;++i) w.push_back(new Worker(boardsize));
-    farm.add_workers(w);
 
     time(&t1);
     printf("Start: \t%s", ctime(&t1));
 
-    if (farm.run_and_wait_end()<0) {
-        error("running farm\n");
-        return -1;
-    }
+    Nqueen(nworkers, depth, boardsize); /* find solutions */
 
     time(&t2);
     printResults(&t1, &t2);
 
-    for(int i=0;i<nworkers;++i) g_numsolutions += ((Worker *)w[i])->getSolutions();
     if (g_numsolutions != 0)
     {
 #ifdef WIN32
@@ -533,12 +531,6 @@ int main(int argc, char** argv) {
     {
         printf("No solutions found.\n");
     }
-    printf("\nFastFlow's stats:\n");
-    for(int i=0;i<nworkers;++i)
-        printf("Worker id=%d, nsol=%lld\n", 
-               i, ((Worker *)w[i])->getSolutions());
-    printf("Stream Len= %ld\n", streamlen);
-    printf("Time: %g (ms)\n", farm.ffTime());
-    farm.ffStats(std::cout);
+
     return 0;
 }
